a7452b0c8534d7ad39c09d5e0841604bc2a9f87f
zhuzhiwei <195286381@qq.com>
2024-11-19 15:01:01 +0800

    feat(cpic): 添加人保AI模块
    - 新增 uni-cpic 模块，用于处理人保业务

---
+++ backend/uni-cpic/src/main/java/com/uni/cpic/CpicTestController.java
@@ -0,0 +1,22 @@
+package com.uni.cpic;
+
+import com.uni.common.core.controller.BaseController;
+import com.uni.common.core.domain.AjaxResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuzw
+ * @date 2024/11/19 14:59
+ */
+@RestController
+@RequestMapping("/cpic/test")
+@Slf4j
+public class CpicTestController extends BaseController {
+    @GetMapping("/hello")
+    public AjaxResult hello(){
+        return success("hello");
+    }
+}
